a215fbe08dda2f7f4c855a7e5d852b15e8852ca6
alphakotenok <kamanton3@gmail.com>
2023-05-10 02:17:28 +0200

    Entity Added

--- src/Model/Entity.java
+++ src/Model/Entity.java
@@ -1,8 +1,29 @@
 package Model;
 
+import javafx.scene.image.Image;
+
 public class Entity {
     public static enum EntityType {
-        cultist, monster, GOO
+        cultist, monster, GOO, none
     };
 
+    public final EntityType entityType;
+    public final Image icon;
+    public final Factions.Faction faction;
+    public final String name;
+
+    public Entity() {
+        entityType = EntityType.none;
+        icon = null;
+        faction = Factions.Faction.none;
+        name = "";
+    }
+
+    public int getCost() {
+        return 0;
+    }
+
+    public int getCombat() {
+        return 0;
+    }
 }
