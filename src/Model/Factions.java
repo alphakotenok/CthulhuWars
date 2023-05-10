a2f2f2922e5b8242b37d29181b446c2b2fd7e9a6
alphakotenok <kamanton3@gmail.com>
2023-05-10 02:17:11 +0200

    Entity added

---
+++ src/Model/Factions.java
@@ -0,0 +1,7 @@
+package Model;
+
+public class Factions {
+    public static enum Faction {
+        GreatCthulhu, CrawlingChaos, BlackGoat, YellowSign, OpenerOfTheWay, Sleeper, Windwalker, none
+    };
+}
