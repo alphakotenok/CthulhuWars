7282f0741693e084a265d1ee94e4fe02948092be
Yuliia Tatarinova <julia.tatar05@gmail.com>
2023-05-28 21:50:53 +0200

    add Entity on maps

--- src/Controler/CommandButton.java
+++ src/Controler/CommandButton.java
@@ -1,7 +1,9 @@
 package Controler;
 
+import Model.Location;
 import Model.Variables;
 import View.ButtonVisualizer;
+import View.EntityVisualizer;
 import View.Misc;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -14,9 +16,15 @@ public class CommandButton implements EventHandler<ActionEvent> {
     }
 
     public void handle(ActionEvent arg0) {
-        Variables.core.activateCommand(commandID);
         Misc.removeButtons(CommandButton.class);
+        Misc.removeLabel(Variables.core.getCommandDescription());
+        Variables.core.activateCommand(commandID);
         ButtonVisualizer.displayCommandButtons();
+
+        for (Location location : Variables.core.getLocationsList()) {
+            EntityVisualizer.removeEntitiesFromMap(location);
+            EntityVisualizer.placeEntitiesOnMap(location);
+        }
         return;
     }
 
