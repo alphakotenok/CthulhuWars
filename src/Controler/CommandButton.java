ca761e7dcbcd780ebeefbb4a39ad0ea861519248
Yuliia Tatarinova <julia.tatar05@gmail.com>
2023-05-31 17:34:31 +0200

    added arrows

--- src/Controler/CommandButton.java
+++ src/Controler/CommandButton.java
@@ -1,8 +1,11 @@
 package Controler;
 
+import java.io.FileNotFoundException;
+
 import Model.Variables;
 import View.ButtonVisualizer;
 import View.EntityVisualizer;
+import View.ImageMisc;
 import View.Visualizer;
 import View.ActionsMisc;
 import javafx.event.ActionEvent;
@@ -17,6 +20,12 @@ public class CommandButton implements EventHandler<ActionEvent> {
 
     public void handle(ActionEvent arg0) {
         ActionsMisc.removeButtons(CommandButton.class);
+        try {
+            ActionsMisc.removeImage(ImageMisc.getArrowImage(Variables.core.getWay()));
+        } catch (FileNotFoundException e) {
+
+            e.printStackTrace();
+        }
         ActionsMisc.removeLabelById("Power label");
         ActionsMisc.removeLabelById("Doom label");
         ActionsMisc.removeLabelById("ElderSign label");
@@ -26,7 +35,11 @@ public class CommandButton implements EventHandler<ActionEvent> {
         ButtonVisualizer.displayCommandButtons();
         Visualizer.initializeLabels();
         Visualizer.displayRitualLabel();
-
+        try {
+            Visualizer.arrowImageView();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         EntityVisualizer.removeEntitiesFromMap();
         EntityVisualizer.placeEntitiesOnMap();
         return;
